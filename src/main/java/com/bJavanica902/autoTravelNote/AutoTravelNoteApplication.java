68e642a752cee901c99db3715eae6d61dcaff0fb
zekey <zeke.yang@rawstone.com.tw>
2024-09-18 16:56:37 +0800

    Initial commit

---
+++ src/main/java/com/bJavanica902/autoTravelNote/AutoTravelNoteApplication.java
@@ -0,0 +1,13 @@
+package com.bJavanica902.autoTravelNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTravelNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTravelNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bJavanica902/autoTravelNote/AutoTravelNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bJavanica902.autoTravelNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTravelNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
